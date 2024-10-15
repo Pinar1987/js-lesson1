39e8e7015b7f0268023d88138bba906a4c675b5e
Pinar1987 <pinarbildirici87@gmail.com>
2024-10-15 14:25:00 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// Prints Hello World to the console //
+console.log("Hello World");
+
+// Declare variables for name, age, and favorite color
+let name = "Pinar";
+let age = "37";
+let favoriteColor = "pink";
+
+
+// Use console.log() to print a sentence including the variables
+console.log("My name is " + name + ", I am " + age + " years old, and my favorite color is " + favoriteColor + ".");
+
+// Declare two number variables
+let num1 = 10;
+let num2 = 5;
+
+// Perform arithmetic operations
+let addition = num1 + num2;         // Addition
+let subtraction = num1 - num2;      // Subtraction
+let multiplication = num1 * num2;   // Multiplication
+let division = num1 / num2;         // Division
+
+// Output the results of the arithmetic operations
+console.log("Addition: " + addition);            // Outputs 15
+console.log("Subtraction: " + subtraction);      // Outputs 5
+console.log("Multiplication: " + multiplication); // Outputs 50
+console.log("Division: " + division);            // Outputs 2
+
+
